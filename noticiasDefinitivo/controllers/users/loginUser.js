98b5ba84e61d21a9b883fb2e4d121a61cd392afd
Iris Morales <moralesortegairis@gmail.com>
2023-06-15 11:08:28 +0200

    cambio Iris

--- noticiasDefinitivo/controllers/users/loginUser.js
+++ noticiasDefinitivo/controllers/users/loginUser.js
@@ -42,6 +42,8 @@ const loginUser = async (req, res, next) => {
     // Objeto con información útil del usuario que guardaremos en el token
     const tokenInfo = {
       id: username[0].id,
+      username: username[0].username,
+      email: username[0].email
     };
 
     // Creamos el token
