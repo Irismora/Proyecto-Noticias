f8b5f483720679af26fd5c00fce1fb45649b683c
Carlota <carlotapicabia@hotmail.com>
2023-07-03 14:29:10 +0200

    codigo de editar

--- frontEnd/src/components/UserNews.jsx
+++ frontEnd/src/components/UserNews.jsx
@@ -3,12 +3,18 @@ import { ErrorMessage } from "./ErrorMessage";
 import { NoticiaList } from "./NoticiaList";
 
 const UserNews = ({ id }) => {
-  const { news, loading, error, removeNoticia } = useNews(id);
+  const { news, loading, error, removeNoticia, EditNewPage } = useNews(id);
 
   if (loading) return <p>Cargando noticias...</p>;
   if (error) return <ErrorMessage message={error} />;
 
-  return <NoticiaList news={news} removeNoticia={removeNoticia} />;
+  return (
+    <NoticiaList
+      news={news}
+      removeNoticia={removeNoticia}
+      EditNewPage={EditNewPage}
+    />
+  );
 };
 
 export default UserNews;
--- frontEnd/src/pages/HomePage.jsx
+++ frontEnd/src/pages/HomePage.jsx
@@ -4,21 +4,25 @@ import { AuthContext } from "../context/AuthContext";
 import { ErrorMessage } from "../components/ErrorMessage";
 import { NewNew } from "../components/NewNew";
 import { NoticiaList } from "../components/NoticiaList";
+import { EditNewPage } from "./EditNewPage";
 
 const HomePage = () => {
   const { news, loading, error, addNew, removeNoticia } = useNews();
   const { user } = useContext(AuthContext);
   if (loading) return <p>cargando noticias....</p>;
   if (error) return <ErrorMessage message={error} />;
- console.log(news);
+  console.log(news);
   return (
     <section>
       {user ? <NewNew addNew={addNew} /> : null}
       <h1>Ultimas Noticias</h1>
-      <NoticiaList news={news} removeNoticia={removeNoticia} />
+      <NoticiaList
+        news={news}
+        removeNoticia={removeNoticia}
+        EditNewPage={EditNewPage}
+      />
     </section>
   );
 };
 
 export default HomePage;
-
